2deb6c5f6ba88b401a1487a1223815ab687a6e94
Mehtab Khan <khanmehtab7864@gmail.com>
2026-06-11 11:32:05 +0530

    Corrected typo in code

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(const int argc, const char* argv[])
 {
     if(!argc <=4)
     {
-        std::err << "Invalid commands";
+        std::cerr << "Invalid commands";
     }
     std::string argv1 = argv[1];
     std::string argv2 = argv[2];
